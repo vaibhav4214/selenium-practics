500d34ed7587d2b3782567f6fe178ce07d7a68a4
Vaibhav Yadav <vaibhavyadav7777777@gmail.com>
2025-06-18 08:50:56 +0530

    methods add

--- src/hellow/hellows.java
+++ src/hellow/hellows.java
@@ -18,11 +18,20 @@ public class hellows {
 			fdriver.get("http://www.facebook.com");
 			fdriver.manage().window().maximize();
 			fdriver.findElement(By.id("email")).sendKeys("yadav@2345");
+			System.out.println(fdriver.findElement(By.id("email")).getTagName());
+			System.out.println(fdriver.findElement(By.id("email")).getText());
+			System.out.println(fdriver.findElement(By.id("email")).getAttribute("type"));
+			
+
 			fdriver.findElement(By.id("pass")).sendKeys("yadav@2345");
+			
 			fdriver.findElement(By.name("login")).click();
-			fdriver.navigate().to("http://www.google.com");
-//			fdriver.navigate().back();			
-			System.out.println(fdriver.getTitle()); //return current web page title
+//			fdriver.findElement(By.id("email")).sendKeys("yadav@2345");
+//			fdriver.navigate().refresh();		//refresh page
+//			fdriver.navigate().to("http://www.google.com");
+			
+//			fdriver.navigate().back();	
+//			fdriver.navigate().forward()	;		System.out.println(fdriver.getTitle()); //return current web page title
 
 //			System.out.println(fdriver.getCurrentUrl()); get current url from browser
 //			fdriver.close()	;
